71fd793e290a6f732c5cd5d3981ff572eea4f89e
Subhajit Chatterjee <subhajt.bdk@gmail.com>
2023-06-18 16:25:04 +0530

    Frontend Development-1

--- mainproject/mainproject/settings.py
+++ mainproject/mainproject/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#added manually
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
